ab8bdee8bc8f2b91c1af7696c09492ca545e78f1
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-04-10 15:59:20 +0100

    ESLint and package setup

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jasmine: true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "paths": ["src"]
+      }
+    }
+  }
+};
