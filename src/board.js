34c5c5236a899fdc5b9e81a576549795560f2e27
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-04-18 23:06:12 +0100

    Unit & feature test cell death circumstances and small refactoring

--- src/board.js
+++ src/board.js
@@ -10,7 +10,6 @@ class Board {
     const neighbours = this.getNeighbours(x, y);
     let alive = 0;
     for (let i = 0; i < neighbours.length; i += 1) {
-      // console.log('should reproduce neighbours: ', neighbours, i);
       if (neighbours[i].isAlive() === true) {
         alive += 1;
       }
@@ -31,72 +30,36 @@ class Board {
       }
     }
     return alive > 3 || alive < 2;
-
   }
 
   getCell(x, y) {
-    // console.log('Get cell - ---------------------');
-    // console.log('I am the problematic board:', this.board);
-    // console.log('My length is: ', this.board.length);
-    // console.log('X and y are: ', x, y, 'respectively');
-    // console.log('End of Get cell -------------');
     return this.board[x][y];
   }
 
   getNeighbours(x, y) {
-    // console.log('I am the problematic board:', this.board);
-    // console.log('My length is: ', this.board.length);
-    // console.log('X and y are: ', x, y, 'respectively');
     const neighbours = [];
-    // console.log('Line 31 neighbours is: ', neighbours);
-    const xValues = this.xValues(x);
-    const yValues = this.yValues(y);
-    // console.log('switch x is: ', x);
-    // console.log('x and y values are', xValues, yValues, 'respectively');
+    const xValues = this.axisNeighbours(x);
+    const yValues = this.axisNeighbours(y);
     for (let i = 0; i < xValues.length; i += 1) {
       for (let j = 0; j < yValues.length; j += 1) {
-        // console.log('i is: ', i);
-        // console.log('j is: ', j);
         if (!(xValues[i] === x && yValues[j] === y)) {
-          // console.log('triggered by i and j of ', i, j);
-          // console.log('xValues', xValues);
-          // console.log('yValues', yValues);
-          // console.log('getCell', this.getCell(xValues[i], yValues[j]));
           neighbours.push(this.getCell(xValues[i], yValues[j]));
-          // console.log('neighbours after pushing i and j: ', i, j, neighbours);
         }
       }
     }
     return neighbours;
   }
 
-  xValues(x) {
-    // console.log('xValues function ------------------')
-    // console.log('x value is ', x);
-    let xValues;
-    if (x < 1) {
-      // console.log('x less than one triggered!');
-      xValues = [x, x + 1];
-      // console.log('case xvalues is: ', xValues);
-    } else if (x === this.board.length - 1) {
-      xValues = [x - 1, x];
+  axisNeighbours(coordinate) {
+    let axisNeighbours;
+    if (coordinate < 1) {
+      axisNeighbours = [coordinate, coordinate + 1];
+    } else if (coordinate === this.board.length - 1) {
+      axisNeighbours = [coordinate - 1, coordinate];
     } else {
-      xValues = [x - 1, x, x + 1];
+      axisNeighbours = [coordinate - 1, coordinate, coordinate + 1];
     }
-    // console.log('xValues in the xValues function are: ', xValues);
-    return xValues;
-  }
-
-  yValues(y) {
-    let yValues;
-    if (y < 1) {
-      yValues = [y, y + 1];
-    } else if (y === this.board.length - 1) {
-      yValues = [y - 1, y];
-    } else {
-      yValues = [y - 1, y, y + 1];
-    }
-    return yValues;
+    return axisNeighbours;
   }
 
   allDead() {
